b1d2378c9e9c265bd982756d458389a5fb8ed084
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-02-17 13:42:11 +0000

    initial commit

---
+++ Desktop/comp30670/setup.py
@@ -0,0 +1,19 @@
+'''
+Created on 17 Feb 2018
+
+@author: aoifeosullivan
+'''
+from setuptools import setup
+
+setup (name="systeminfo",
+       version="0.1",
+       description="Basic system information for COMP30670",
+       url="",
+       author="Aoife O Sullivan",
+       author_email="aoifeosullivan19@gmail.com",
+       license="GPL3",
+       packages=['systeminfo'],
+       entry_points={
+           'console_scripts':['Practicals_systeminfo=systeminfo.main:main']
+           }
+       )
